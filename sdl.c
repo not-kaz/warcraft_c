122750864f8071749cc70fbdc3c1b79057e52159
not-kaz <kazislav@protonmail.com>
2023-02-26 11:51:24 +0100

    Implemented rudimentary SDL initialization and error handling.

--- sdl.c
+++ sdl.c
@@ -2,7 +2,8 @@
 #include "log.h"
 
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_render.h>
+
+#define SDL_INIT_FLAG (SDL_INIT_VIDEO | SDL_INIT_AUDIO)
 
 static struct {
 	SDL_Window *window;
@@ -11,6 +12,25 @@ static struct {
 
 void sdl_setup()
 {
+	if ((SDL_WasInit(SDL_INIT_FLAG) & SDL_INIT_FLAG)
+			|| (sdl_ctx.window && sdl_ctx.renderer)) {
+		LOG_DEBUG("Attempted to setup SDL contexts again.");
+		return;
+	}
+	if (SDL_Init(SDL_INIT_FLAG) != 0) {
+		LOG_ERROR("Failed to initialize SDL. %s", SDL_GetError());
+		exit(EXIT_FAILURE);
+	}
+	sdl_ctx.window = SDL_CreateWindow("Warcraft", 0, 0, 1024, 720, 0);
+	if (!sdl_ctx.window) {
+		LOG_ERROR("Failed to create SDL window. %s", SDL_GetError());
+		exit(EXIT_FAILURE);
+	}
+	sdl_ctx.renderer = SDL_CreateRenderer(sdl_ctx.window, -1, 0);
+	if (!sdl_ctx.renderer) {
+		LOG_ERROR("Failed to create SDL renderer. %s", SDL_GetError());
+		exit(EXIT_FAILURE);
+	}
 }
 
 void sdl_present_frame()
@@ -25,4 +45,5 @@ void sdl_clear_frame()
 
 void sdl_set_draw_color(uint8_t r, uint8_t g, uint8_t b)
 {
+
 }
