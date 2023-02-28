57e7cbc83a076cff142b70ab158642800f2c05e8
not-kaz <kazislav@protonmail.com>
2023-02-28 15:33:02 +0100

    Modified main for testing purposes.

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
+#include "sdl.h"
+
 #define UNUSED(x) (void)(x)
 
 int main(int argc, char **argv)
 {
+	int ww, wh;
+
 	UNUSED(argc);
 	UNUSED(argv);
+	sdl_setup();
+	sdl_get_window_size(&ww, &wh);
+	while (1) {
+		sdl_set_draw_color(0, 0, 0);
+		sdl_clear_frame();
+		sdl_present_frame();
+	}
 	return 0;
 }
